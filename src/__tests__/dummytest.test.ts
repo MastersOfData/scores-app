99c28dc1afde21fb73205280ab825daf3f2e57c9
584975 <584975@stud.hvl.no>
2023-02-21 14:10:51 +0100

    Dummytest finito

--- src/__tests__/dummytest.test.ts
+++ src/__tests__/dummytest.test.ts
@@ -1,7 +1,3 @@
-import React from "react";
-
-describe("dummy", () => {
-  test("1 + 1 equals 2", () => {
-    expect(1 + 1).toBe(2);
-  });
+test("1 + 1 equals 2", () => {
+  expect(1 + 1).toBe(2);
 });
