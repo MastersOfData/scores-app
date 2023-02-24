afacf7f6ded6dae9371629b05d8ab7a9396623fc
h584974 <oliveroloughlin@hotmail.com>
2023-02-24 10:46:32 +0100

    replaced head.js with Metadata API

---
+++ src/app/error.tsx
@@ -0,0 +1,4 @@
+
+export default function HomePageError() {
+  return <h1>Oops, something went wrong</h1>
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,13 @@
+import type { Metadata } from "next"
 import "../styles/globals.css"
+
+export const metadata: Metadata = {
+  title: "Score Tracker"
+}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <head />
       <body>{children}</body>
     </html>
   )
