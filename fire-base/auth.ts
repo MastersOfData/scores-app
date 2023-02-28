62c7fc8c7c51547dfbd05a6394caf1b99f5835bf
h584974 <oliveroloughlin@hotmail.com>
2023-02-28 14:02:57 +0100

    Fixed firebase auth imports, Google sign in broken

--- fire-base/auth.ts
+++ fire-base/auth.ts
@@ -1,8 +1,22 @@
 import { auth } from "./services"
-import { signInWithEmailAndPassword, signOut as _signOut, createUserWithEmailAndPassword, onAuthStateChanged as _onAuthStateChanged, NextOrObserver, User } from "firebase/auth/cordova"
+import { 
+  signInWithEmailAndPassword, 
+  signOut as _signOut, 
+  signInWithPopup, 
+  createUserWithEmailAndPassword, 
+  onAuthStateChanged as _onAuthStateChanged, 
+  NextOrObserver, 
+  User, 
+  GoogleAuthProvider } from "firebase/auth"
 
 export async function signIn(email: string, password: string) {
   return await signInWithEmailAndPassword(auth, email, password)
+}
+
+export async function signInWithGoogle() {
+  const provider = new GoogleAuthProvider()
+  provider.addScope("email")
+  return await signInWithPopup(auth, provider)
 }
 
 export async function signOut() {
--- src/components/AuthTester.tsx
+++ src/components/AuthTester.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useEffect } from "react"
-import { signIn, createAccount, signOut, onAuthStateChanged } from "../../firebase/auth"
+import { signIn, createAccount, signOut, onAuthStateChanged, signInWithGoogle } from "../../fire-base/auth"
 
 export default function AuthTester() {
 
@@ -13,6 +13,7 @@ export default function AuthTester() {
     <div style={{display: "flex", gap: ".5rem"}}>
       <button onClick={() => createAccount("oliveroloughlin1@gmail.com", "pass123")}>Create account</button>
       <button onClick={() => signIn("oliveroloughlin1@gmail.com", "pass123")}>Sign in</button>
+      <button onClick={signInWithGoogle}>Sign in with Google</button>
       <button onClick={signOut}>Sign out</button>
     </div>
   )
