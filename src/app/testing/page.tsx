9b63b65c21af7d3901e47e3132b8fb02c6b79c5e
andersbuanes <anderstbuanes@hotmail.com>
2023-03-01 15:47:10 +0100

    Add button and icons

--- src/app/testing/page.tsx
+++ src/app/testing/page.tsx
@@ -1,5 +1,11 @@
-import AuthTester from "../../components/AuthTester"
-import styles from "../../styles/Testing.module.css"
+"use client";
+
+import { ControllerIcon } from "src/assets/icons/ControllerIcon";
+import { GroupIcon } from "src/assets/icons/GroupIcon";
+import { PersonIcon } from "src/assets/icons/PersonIcon";
+import { Button, ButtonColor, ButtonVariant } from "src/components/Button";
+import AuthTester from "../../components/AuthTester";
+import styles from "../../styles/Testing.module.css";
 
 export default function TestingPage() {
   return (
@@ -21,19 +27,30 @@ export default function TestingPage() {
 
       <div className="form-group">
         <form className={styles.form}>
-          <input
-            type="text"
-            placeholder="Text Input"
-          />
-          <textarea 
-            rows={6}
-            placeholder="Textarea"
-          />
+          <input type="text" placeholder="Text Input" />
+          <textarea rows={6} placeholder="Textarea" />
           <button>Submit Button</button>
         </form>
       </div>
-
+      <Button  variant={ButtonVariant.Action}>
+        <ControllerIcon />
+      </Button>
+      <Button  variant={ButtonVariant.Action} color={ButtonColor.Red}>
+        <GroupIcon />
+      </Button>
+      <Button  variant={ButtonVariant.Action} color={ButtonColor.Pink}>
+        <PersonIcon />
+      </Button>
+      <Button variant={ButtonVariant.Small}>
+        Liten knapp
+      </Button>
+      <Button variant={ButtonVariant.Medium}>
+        Medium knapp
+      </Button>
+      <Button variant={ButtonVariant.Round} color={ButtonColor.Pink}>
+        Rund knapp
+      </Button>
       <AuthTester />
     </main>
-  )
+  );
 }
---
+++ src/components/Button.tsx
@@ -0,0 +1,44 @@
+import React, { ButtonHTMLAttributes, FC, ReactNode } from "react";
+import styles from "../styles/Button.module.css";
+
+export enum ButtonColor {
+  Green = "button--green",
+  Red = "button--red",
+  Yellow = "button--yellow",
+  Pink = "button--pink",
+  Orange = "button--orange",
+}
+
+export enum ButtonVariant {
+  Small = "button-sm",
+  Medium = "button-md",
+  Round = "button-round",
+  Action = "button-action",
+}
+
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variant: ButtonVariant;
+  color?: ButtonColor;
+  className?: string;
+  onClick?: () => void;
+  children?: ReactNode;
+}
+
+export const Button: FC<ButtonProps> = (props) => {
+  const {
+    variant,
+    color = ButtonColor.Green,
+    className = "",
+    onClick,
+    children,
+  } = props;
+
+  return (
+    <button
+      className={`${styles[variant]} ${styles[color]} ${className}`}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
