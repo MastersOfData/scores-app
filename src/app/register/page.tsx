1868ca2a9f2e6a4eb44c942b843255530fd72489
andersbuanes <anderstbuanes@hotmail.com>
2023-03-04 10:19:53 +0100

    finish register page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,55 +1,60 @@
 "use client";
-
+import { useState } from "react";
 import { useRouter } from "next/navigation";
 
-import { GamesIcon } from "src/assets/icons/GamesIcon";
+import { createAccount } from "fire-base/auth";
+import { GamesBannerIcon } from "src/assets/icons/GamesBannerIcon";
 import { Button, ButtonVariant } from "src/components/Button";
 import Input from "src/components/Input";
 import styles from "../../styles/Register.module.css";
 
 interface RegisterFields {
   email: string;
-  username: string;
   password: string;
 }
 
 export default function RegisterPage() {
   const router = useRouter();
 
-  const onSubmit = async (data: RegisterFields) => {
-    console.log(data);
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const onSubmit = async ({ email, password }: RegisterFields) => {
+    createAccount(email, password)
+      .catch((err) => alert(`Failed with error: ${err}`)) // temp
+      .then(() => router.push("/"));
   };
 
   return (
     <main className={styles.container}>
       <h1>Registrer</h1>
-      <GamesIcon />
+      <div className={styles.banner}>
+        <GamesBannerIcon />
+      </div>
       <div className="form-group">
         <form
           className={styles.form}
-          onSubmit={() =>
-            onSubmit({
-              email: "mock@mock.com",
-              username: "mock",
-              password: "mockito123",
-            })
-          }
+          onSubmit={() => onSubmit({ email: email, password: password })}
         >
           <div className={styles["label-input-group"]}>
             <label>E-postadresse:</label>
-            <Input type="email" placeholder="Skriv e-postadresse..." />
-          </div>
-          <div className={styles["label-input-group"]}>
-            <label>Brukernavn:</label>
-            <Input type="text" placeholder="Skriv brukernavn..." />
+            <Input
+              type="email"
+              placeholder="Skriv e-postadresse..."
+              onInput={setEmail}
+            />
           </div>
           <div className={styles["label-input-group"]}>
             <label>Passord:</label>
-            <Input type="password" placeholder="Skriv passord..." />
+            <Input
+              type="password"
+              placeholder="Skriv passord..."
+              onInput={setPassword}
+            />
           </div>
           <Button
             variant={ButtonVariant.Round}
-            className={styles["button--submit"]}
+            className={styles["button-container"]}
           >
             Registrer
           </Button>
