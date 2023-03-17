36908bbdb093c6aed3e876a976aecaf23453b3fb
h584974 <oliveroloughlin@hotmail.com>
2023-03-17 13:55:36 +0100

    account page branch

---
+++ src/app/account/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import { useHeader } from "src/components/Header"
+
+export default function AccountPage() {
+  useHeader("Account", "/")
+  return (
+    <main>
+      
+    </main>
+  )
+}
