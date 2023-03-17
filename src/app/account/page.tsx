4ad1985700c1e9b41c70eb960649d2e24c356baf
h584974 <oliveroloughlin@hotmail.com>
2023-03-17 15:33:15 +0100

    save work on account page

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -3,6 +3,10 @@
 import { useHeader } from "src/components/Header"
 import Input from "src/components/Input"
 import { useState, FormEvent } from "react"
+import { validatePassword } from "src/utils/acccount"
+import { getCurrentUser, updatePassword } from "src/fire-base/auth"
+import styles from "src/styles/Account.module.css"
+import { Button, ButtonColor, ButtonVariant } from "src/components/Button"
 
 export default function AccountPage() {
   useHeader("Profil", "/")
@@ -11,23 +15,48 @@ export default function AccountPage() {
   const [confPassword, setConfPassword] = useState<string>("")
 
   async function handleSubmit(e: FormEvent) {
-    
+    e.preventDefault()
+
+    if (confPassword !== password) {
+      // TODO: Inform about mismatch
+      return
+    }
+
+    const { valid, error } = validatePassword(password)
+    if (!valid) {
+      // TODO: Inform about error
+      return
+    }
+
+    const user = getCurrentUser()
+    if (!user) {
+      // TODO: Redirect to sign in?
+      return
+    }
+
+    await updatePassword(user, password)
   }
 
   return (
-    <main>
-      <form>
-        <strong>Endre password</strong>
-        <Input
-          type="password"
-          placeholder="Nytt passord"
-          onInput={setPassword}
-        />
+    <main className={styles.main}>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <div>
+          <strong>Endre password</strong>
+          <Input
+            type="password"
+            placeholder="Nytt passord"
+            onInput={setPassword}
+          />
+        </div>
         <Input
           type="password"
           placeholder="Bekreft passord"
           onInput={setConfPassword}
         />
+        <Button variant={ButtonVariant.Round} color={ButtonColor.Green}>Endre passord</Button>
+        <span className={styles["button-container"]}>
+          <Button variant={ButtonVariant.Medium} color={ButtonColor.Red}>Logg ut</Button>
+        </span>
       </form>
     </main>
   )
--- src/utils/acccount.ts
+++ src/utils/acccount.ts
@@ -1,26 +1,31 @@
 
 export type PasswordValidation = {
-  result: boolean,
+  valid: boolean,
   error?: string
 }
 
 export function validatePassword(password: string): PasswordValidation {
   if (password.length < 8 || password.length > 64) return {
-    result: false,
+    valid: false,
     error: "Password must be between 8 and 64 characters long"
   }
 
-  if (!/[a-z]+/.test(password)) return {
-    result: false,
+  if (!/[a-zøæå]+/.test(password)) return {
+    valid: false,
     error: "Password must contain at least one lower case character"
   }
 
-  if (!/[A-Z]+/.test(password)) return {
-    result: false,
+  if (!/[A-ZØÆÅ]+/.test(password)) return {
+    valid: false,
     error: "Password must contain at least one upper case character"
   }
 
+  if (!/[0-9]+/.test(password)) return {
+    valid: false,
+    error: "Password must contain at least one digit"
+  }
+
   return {
-    result: true
+    valid: true
   }
 }
