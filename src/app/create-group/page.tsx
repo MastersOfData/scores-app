97a4077fa631034f7effb09341adcc19273b6fca
Rune Mæstad <54098894+rune-m@users.noreply.github.com>
2023-03-21 18:57:25 +0100

    Small fixes (#47)
    - Added PageWrapper on create-group page
    - Removed old useHeader-import
    - Styling fixes
    - Added old font-import until new is fixed

--- src/app/create-group/page.tsx
+++ src/app/create-group/page.tsx
@@ -1,59 +1,56 @@
 "use client";
 
 import styles from "../../styles/CreateGroupe.module.css";
-import { useHeader } from "src/components/Header";
 import Input from "src/components/Input";
 import { FormEvent, useState } from "react";
 import { Button, ButtonVariant, ButtonColor } from "src/components/Button";
+import PageWrapper from "../../components/PageWrapper";
 
-export default function CreateGroupPage(){  
-    const [groupName, setGroupName] = useState<string>("");
-    const [emoji, setEmoji] = useState<string>("");
-    useHeader("Ny Gruppe", "/")
-    
+export default function CreateGroupPage() {
+  const [groupName, setGroupName] = useState<string>("");
+  const [emoji, setEmoji] = useState<string>("");
 
-    async function onSubmit(e: FormEvent) {
-        e.preventDefault();   
-    }
+  async function onSubmit(e: FormEvent) {
+    e.preventDefault();
+  }
 
-    return (
-        <main className = {styles.container}>
-          <div>
-            <form className={styles.form}>
-              <div className={styles.inputContainer}>
-                <p className={styles.inputLabel}>Gruppenavn:</p>
-                  <Input 
-                    className={styles.inputStyle}
-                    type = "text"
-                    placeholder="Skriv gruppenavn..."
-                    onInput={setGroupName}
-                  />
-              </div>
-              <div className={styles.inputContainer}>
-                <p className={styles.inputLabel}>Emoji:</p>
-                        
-                </div>
-                <div className={styles.emojiContainer}>
-                  <Input
-                    className={styles.emojiStyle}
-                    type="text"
-                    placeholder=""
-                    onInput={setEmoji}
-                    maxLength={2}
-                  />
-                </div> 
-                <div>
-                  <Button
-                    className={styles["button-container"]}
-                    variant={ButtonVariant.Round}
-                    color={ButtonColor.Green}
-                    onSubmit={e => onSubmit(e)}
-                  >
-                    Opprett gruppe
-                  </Button>
-                </div>
-            </form>
+  return (
+    <PageWrapper title='Ny gruppe' backPath='/'>
+      <div>
+        <form className={styles.form}>
+          <div className={styles.inputContainer}>
+            <p className={styles.inputLabel}>Gruppenavn:</p>
+            <Input
+              className={styles.inputStyle}
+              type='text'
+              placeholder='Skriv gruppenavn...'
+              onInput={setGroupName}
+            />
           </div>
-        </main>
-    )
+          <div className={styles.inputContainer}>
+            <p className={styles.inputLabel}>Emoji:</p>
+          </div>
+          <div className={styles.emojiContainer}>
+            <Input
+              className={styles.emojiStyle}
+              type='text'
+              placeholder=''
+              onInput={setEmoji}
+              maxLength={2}
+            />
+          </div>
+          <div className='center-items'>
+            <Button
+              className={styles["button-container"]}
+              variant={ButtonVariant.Round}
+              color={ButtonColor.Green}
+              onSubmit={(e) => onSubmit(e)}
+            >
+              Opprett gruppe
+            </Button>
+          </div>
+        </form>
+      </div>
+    </PageWrapper>
+  );
 }
