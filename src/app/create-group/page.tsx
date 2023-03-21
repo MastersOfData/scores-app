b89ac2df87c955ebc5642a9c86cb5df225deeaf2
Erlend.Skutlaberg <erlend.skutlaberg@student.uib.no>
2023-03-21 14:20:18 +0100

    Fixed indentation

--- src/app/create-group/page.tsx
+++ src/app/create-group/page.tsx
@@ -6,8 +6,6 @@ import Input from "src/components/Input";
 import { FormEvent, useState } from "react";
 import { Button, ButtonVariant, ButtonColor } from "src/components/Button";
 
-
-
 export default function CreateGroupPage(){  
     const [groupName, setGroupName] = useState<string>("");
     const [emoji, setEmoji] = useState<string>("");
@@ -20,42 +18,42 @@ export default function CreateGroupPage(){
 
     return (
         <main className = {styles.container}>
-            <div>
-                <form className={styles.form}>
-                    <div className={styles.inputContainer}>
-                        <p className={styles.inputLabel}>Gruppenavn:</p>
-                        <Input 
-                            className={styles.inputStyle}
-                            type = "text"
-                            placeholder="Skriv gruppenavn..."
-                            onInput={setGroupName}
-                             />
-                            </div>
-                    <div className={styles.inputContainer}>
-                        <p className={styles.inputLabel}>Emoji:</p>
+          <div>
+            <form className={styles.form}>
+              <div className={styles.inputContainer}>
+                <p className={styles.inputLabel}>Gruppenavn:</p>
+                  <Input 
+                    className={styles.inputStyle}
+                    type = "text"
+                    placeholder="Skriv gruppenavn..."
+                    onInput={setGroupName}
+                  />
+              </div>
+              <div className={styles.inputContainer}>
+                <p className={styles.inputLabel}>Emoji:</p>
                         
-                    </div>
-                        <div className={styles.emojiContainer}>
-                        <Input
-                            className={styles.emojiStyle}
-                            type="text"
-                            placeholder=""
-                            onInput={setEmoji}
-                            maxLength="2"
-                            />
-                            </div> 
-                        <div>
-                        <Button
-                            className={styles["button-container"]}
-                            variant={ButtonVariant.Round}
-                            color={ButtonColor.Green}
-                            onSubmit={e => onSubmit(e)}
-                            >
-                                Opprett gruppe
-                        </Button>
-                        </div>
-                </form>
-            </div>
+                </div>
+                <div className={styles.emojiContainer}>
+                  <Input
+                    className={styles.emojiStyle}
+                    type="text"
+                    placeholder=""
+                    onInput={setEmoji}
+                    maxLength={2}
+                  />
+                </div> 
+                <div>
+                  <Button
+                    className={styles["button-container"]}
+                    variant={ButtonVariant.Round}
+                    color={ButtonColor.Green}
+                    onSubmit={e => onSubmit(e)}
+                  >
+                    Opprett gruppe
+                  </Button>
+                </div>
+            </form>
+          </div>
         </main>
     )
 }
