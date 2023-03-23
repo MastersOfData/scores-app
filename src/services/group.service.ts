b7fae0c48a3e94751b1d4ab959c59beba4a0807d
Rune Mæstad <rune.maestad@gmail.com>
2023-03-23 19:02:34 +0100

    Lagt til flere actions for å endre på grupper

--- src/services/group.service.ts
+++ src/services/group.service.ts
@@ -1,6 +1,7 @@
 import { documentId, where } from "firebase/firestore";
 import {
   addDocument,
+  deleteDocument,
   getDocument,
   getDocuments,
   groupsCol,
@@ -67,11 +68,79 @@ export const joinGroup = async (groupId: string, userId: string) => {
   return await getDocument<Group>(groupsCol, docId);
 };
 
+export const createGroupNew = async (
+  currentUserId: string,
+  groupName: string,
+  emoji: string
+) => {
+  const group: Group = {
+    name: groupName,
+    emoji: emoji,
+    games: [],
+    invitationCode: "", // TODO: Generate invite code
+    gameTypes: [],
+  };
+  const groupRef = await addDocument(groupsCol, group);
+
+  await joinGroup(groupRef.id, currentUserId);
+  return getGroupInternal(groupRef.id);
+};
+
+export const joinGroupByInvitationCode = async (
+  invitationCode: string,
+  userId: string
+) => {
+  const groupArray = await getDocuments<Group>({
+    collectionId: groupsCol,
+    constraints: [where("invitationCode", "==", invitationCode)],
+  });
+  if (groupArray.length === 0 || groupArray.length > 1) return Promise.reject();
+
+  const groupId = groupArray[0].invitationCode;
+  const docId = generateUserGroupStatisticDocumentId(userId, groupId);
+
+  const userGroupStatistic: UserGroupStatistic = {
+    userId: userId,
+    groupId: groupId,
+    wins: 0,
+    draws: 0,
+    losses: 0,
+  };
+
+  await setDocument(userGroupStatisticsCol, docId, userGroupStatistic);
+
+  return await getGroupInternal(groupId);
+};
+
+export const removeUserFromGroup = async (userId: string, groupId: string) => {
+  const statsId = generateUserGroupStatisticDocumentId(userId, groupId);
+  await deleteDocument(userGroupStatisticsCol, statsId);
+};
+
 export const getStatsForAllUsersInGroup = async (groupId: string) => {
   return await getDocuments<UserGroupStatistic>({
     collectionId: userGroupStatisticsCol,
     constraints: [where("groupId", "==", groupId)],
   }).then((res) => res);
+};
+
+export const getGroupInternal = async (groupId: string) => {
+  const group = await getDocument<Group>(groupsCol, groupId);
+  if (!group) return Promise.reject();
+
+  const stats = await getStatsForAllUsersInGroup(groupId);
+  const users = await getDocuments<User>({
+    collectionId: usersCol,
+    constraints: [
+      where(
+        documentId(),
+        "in",
+        stats.map((s) => s.userId)
+      ),
+    ],
+  });
+
+  return mapGroupAndUsersToGroupInternal(group, stats, users);
 };
 
 export const getGroupsInternalForCurrentUser = async (userId: string) => {
--- src/store/groupsInternal.reducer.ts
+++ src/store/groupsInternal.reducer.ts
@@ -1,5 +1,10 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { getGroupsInternalForCurrentUser } from "../services/group.service";
+import {
+  createGroupNew,
+  getGroupsInternalForCurrentUser,
+  joinGroupByInvitationCode,
+  removeUserFromGroup,
+} from "../services/group.service";
 import { GroupInternal } from "../types/types";
 import { DataStatus, DataWithStatus } from "./store.types";
 
@@ -28,6 +33,46 @@ export const getAllGroupsAction = createAsyncThunk(
   }
 );
 
+export const joinGroupByInvitationCodeAction = createAsyncThunk(
+  "groupsInternal/joinGroup",
+  async ({
+    invitationCode,
+    userId,
+  }: {
+    invitationCode: string;
+    userId: string;
+  }) => {
+    return await joinGroupByInvitationCode(invitationCode, userId);
+  }
+);
+
+export const createGroupAction = createAsyncThunk(
+  "groupsInternal/createGroup",
+  async ({
+    currentUserId,
+    groupName,
+    groupEmoji,
+  }: {
+    currentUserId: string;
+    groupName: string;
+    groupEmoji: string;
+  }) => {
+    const res = await createGroupNew(currentUserId, groupName, groupEmoji);
+    return res;
+  }
+);
+
+export const removeUserFromGroupAction = createAsyncThunk(
+  "groupsInternal/removeUser",
+  async ({ userId, groupId }: { userId: string; groupId: string }) => {
+    await removeUserFromGroup(userId, groupId);
+    return {
+      userId,
+      groupId,
+    };
+  }
+);
+
 const groupsInternalSlice = createSlice({
   name: "groupsInternal",
   initialState: initialState,
@@ -44,6 +89,46 @@ const groupsInternalSlice = createSlice({
       .addCase(getAllGroupsAction.rejected, (state) => {
         state.status = DataStatus.ERROR;
         state.data = [];
+      })
+      .addCase(joinGroupByInvitationCodeAction.pending, (state) => {
+        state.update.status = DataStatus.LOADING;
+      })
+      .addCase(joinGroupByInvitationCodeAction.fulfilled, (state, action) => {
+        state.update.status = DataStatus.COMPLETED;
+        if (state.data) state.data.push(action.payload);
+        else state.data = [action.payload];
+      })
+      .addCase(joinGroupByInvitationCodeAction.rejected, (state) => {
+        state.update.status = DataStatus.ERROR;
+      })
+      .addCase(createGroupAction.pending, (state) => {
+        state.create.status = DataStatus.LOADING;
+      })
+      .addCase(createGroupAction.fulfilled, (state, action) => {
+        state.create.status = DataStatus.COMPLETED;
+        if (state.data) state.data.push(action.payload);
+        else state.data = [action.payload];
+      })
+      .addCase(createGroupAction.rejected, (state) => {
+        state.create.status = DataStatus.ERROR;
+      })
+      .addCase(removeUserFromGroupAction.pending, (state, action) => {
+        state.update.status = DataStatus.LOADING;
+        state.update.dataId = action.meta.arg.groupId;
+      })
+      .addCase(removeUserFromGroupAction.fulfilled, (state, action) => {
+        state.update.status = DataStatus.COMPLETED;
+        state.update.dataId = undefined;
+        state.data?.forEach((group) => {
+          group.id === action.payload.groupId
+            ? group.members.filter(
+                (member) => member?.id === action.payload.userId
+              )
+            : group;
+        });
+      })
+      .addCase(removeUserFromGroupAction.rejected, (state) => {
+        state.update.status = DataStatus.ERROR;
       });
   },
 });
