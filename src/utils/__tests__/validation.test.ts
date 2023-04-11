41a7e3bf6f9d322e7278affade764fd99781ebfa
oliver-oloughlin <oliveroloughlin@hotmail.com>
2023-04-11 09:52:34 +0200

    Improved password valdiation test

--- src/utils/__tests__/validation.test.ts
+++ src/utils/__tests__/validation.test.ts
@@ -1,11 +1,24 @@
 import { validatePassword } from "../validation";
 
-test("validatePassword", () => {
-  expect(validatePassword("abc").valid).toBe(false)
-  expect(validatePassword("hdcsiuhpIfoifhdsoh823y4h2hdoqchocgHUHSDUGSLKSHDUIHEUGSHUDHSDHUSGDSUGDSUDGSDGSIDGIUSDGSIUDGSUDGSDGSDUGSDSUIDGSUGIDDy297y3d9").valid).toBe(false)
-  expect(validatePassword("abcd1234").valid).toBe(false)
-  expect(validatePassword("ABCD1234").valid).toBe(false)
-  expect(validatePassword("abcdABCD").valid).toBe(false)
-  expect(validatePassword("abCD1234").valid).toBe(true)
-  expect(validatePassword("æøåABDI425").valid).toBe(true)
+describe("validatePassword", () => {
+  it.each([
+    "abc", 
+    "hdcsiuhpIfoifhdsoh823y4h2hdoqchocgHUHSDUGSLKSHDUIHEUGSHUDHSDHUSGDSUGDSUDGSDGSIDGIUSDGSIUDGSUDGSDGSDUGSDSUIDGSUGIDDy297y3d9",
+    "abcd1234",
+    "abcdABCD"
+  ],
+  )("Should fail validation", password => {
+    const validation = validatePassword(password)
+    expect(validation.valid).toBe(false)
+  })
+
+  it.each([
+    "abcdABCD1234",
+    "20sisduJSDHUI",
+    "æøåABDI425"
+  ],
+  )("Should pass validation", password => {
+    const validation = validatePassword(password)
+    expect(validation.valid).toBe(true)
+  })
 })
