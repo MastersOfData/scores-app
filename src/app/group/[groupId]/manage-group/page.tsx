d24874b38c66e16d1a94bd03f080953c482e2cd9
584975 <584975@stud.hvl.no>
2023-04-12 16:28:17 +0200

    Progress on manage group page

--- src/app/group/[groupId]/manage-group/page.tsx
+++ src/app/group/[groupId]/manage-group/page.tsx
@@ -1,5 +1,13 @@
-import { FC } from "react";
+"use client";
+
+import { FC, useState } from "react";
+import { Button, ButtonColor, ButtonVariant } from "src/components/Button";
+import Input from "src/components/Input";
 import PageWrapper from "src/components/PageWrapper";
+import Spinner from "src/components/Spinner";
+import { useGetGroupsForCurrentUser } from "src/store/hooks";
+import { DataStatus } from "src/store/store.types";
+import styles from "src/styles/ManageGroup.module.css";
 
 interface ManagePageProps {
   params: { groupId: string };
@@ -7,5 +15,45 @@ interface ManagePageProps {
 
 const ManageGroupPage: FC<ManagePageProps> = ({ params }) => {
   const { groupId } = params;
-  return <PageWrapper title="Gruppenavn" backPath="/group"></PageWrapper>;
+  const [username, setUsername] = useState<string | undefined>();
+  const groupsWithStatus = useGetGroupsForCurrentUser();
+
+  if (
+    groupsWithStatus.status === DataStatus.LOADING ||
+    groupsWithStatus.data === undefined
+  ) {
+    return <Spinner />;
+  }
+  const group = groupsWithStatus.data?.find((group) => group.id === groupId);
+
+  const onAdd = async () => {};
+  return (
+    <PageWrapper
+      title={group?.name ?? "Din gruppe"}
+      backPath={`/group/${groupId}`}
+    >
+      <div className={styles.container}>
+        <span className={styles.code}>
+          {`Kode: ${group?.invitationCode ?? ""}`}
+        </span>
+        <div className={styles["input-container"]}>
+          <Input
+            type="text"
+            className={styles["text-input"]}
+            placeholder="Brukernavn..."
+            onInput={setUsername}
+          />
+          <Button
+            variant={ButtonVariant.Medium}
+            color={ButtonColor.Pink}
+            onClick={onAdd}
+          >
+            Legg til
+          </Button>
+        </div>
+      </div>
+    </PageWrapper>
+  );
 };
+
+export default ManageGroupPage;
