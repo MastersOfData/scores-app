d4abc061652f731d75c87d6bfe8f0876c9ecc937
Anders Aarsæther <54100993+AndersAarsaether@users.noreply.github.com>
2023-04-15 18:29:25 +0200

    Administrer gruppe (#81)
    * Almost finished. Some logic missing and bugs

--- src/app/group/[groupId]/manage-group/page.tsx
+++ src/app/group/[groupId]/manage-group/page.tsx
@@ -1,11 +1,16 @@
 "use client";
 
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
+import { RemoveIcon } from "src/assets/icons/RemoveIcon";
 import { Button, ButtonColor, ButtonVariant } from "src/components/Button";
+import { CheckboxCards } from "src/components/CheckboxCards";
 import Input from "src/components/Input";
 import PageWrapper from "src/components/PageWrapper";
 import Spinner from "src/components/Spinner";
-import { useGetGroupsForCurrentUser } from "src/store/hooks";
+import TitleWithInfo from "src/components/TitleWithInfo";
+import { getUserId } from "src/services/user.service";
+import { joinGroupByInvitationCodeAction } from "src/store/groupsInternal.reducer";
+import { useAppDispatch, useGetGroupsForCurrentUser } from "src/store/hooks";
 import { DataStatus } from "src/store/store.types";
 import styles from "src/styles/ManageGroup.module.css";
 
@@ -14,9 +19,18 @@ interface ManagePageProps {
 }
 
 const ManageGroupPage: FC<ManagePageProps> = ({ params }) => {
-  const { groupId } = params;
-  const [username, setUsername] = useState<string | undefined>();
   const groupsWithStatus = useGetGroupsForCurrentUser();
+  const dispatch = useAppDispatch();
+  const { groupId } = params;
+  const group = groupsWithStatus.data?.find((group) => group.id === groupId);
+
+  const [username, setUsername] = useState<string>("");
+  const [removedMembers, setRemovedMembers] = useState<string[]>([]);
+  const [remainingMembers, setRemainingMembers] = useState<string[]>([]);
+
+  useEffect(() => {
+    setRemainingMembers(group?.members.map((member) => member.username) ?? []);
+  }, [group?.members]);
 
   if (
     groupsWithStatus.status === DataStatus.LOADING ||
@@ -24,11 +38,37 @@ const ManageGroupPage: FC<ManagePageProps> = ({ params }) => {
   ) {
     return <Spinner />;
   }
-  const group = groupsWithStatus.data?.find((group) => group.id === groupId);
 
   const onAdd = async () => {
-    console.log("Ikke implementert logikk");
+    if (username && group) {
+      const userId = await getUserId(username);
+      if (userId) {
+        await dispatch(
+          joinGroupByInvitationCodeAction({
+            invitationCode: group.invitationCode,
+            userId: userId,
+          })
+        ).unwrap();
+      }
+      console.log("No user with given username");
+    }
+    setUsername("");
   };
+
+  const onRemove = (username: string) => {
+    const newRemoved = removedMembers;
+    const newRemaining = remainingMembers.filter(
+      (user) => !(user === username)
+    );
+    newRemoved.push(username);
+    setRemovedMembers(newRemoved);
+    setRemainingMembers(newRemaining);
+  };
+
+  const onConfirm = async () => {
+    console.log("Ikke implementert");
+  };
+
   return (
     <PageWrapper
       title={group?.name ?? "Din gruppe"}
@@ -41,6 +81,7 @@ const ManageGroupPage: FC<ManagePageProps> = ({ params }) => {
         <div className={styles["input-container"]}>
           <Input
             type="text"
+            value={username}
             className={styles["text-input"]}
             placeholder="Brukernavn..."
             onInput={setUsername}
@@ -53,6 +94,70 @@ const ManageGroupPage: FC<ManagePageProps> = ({ params }) => {
             Legg til
           </Button>
         </div>
+        {remainingMembers.length > 0 && (
+          <div className={styles["members-list-container"]}>
+            <TitleWithInfo
+              title="Medlemmer"
+              infoText="Fjern medlemmer ved å trykke på rødt skilt"
+            />
+            <table className={styles["leaderboard"]}>
+              <thead>
+                <tr>
+                  <th className={styles["text-align-left"]}>Medlem</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                {remainingMembers.map((username, i) => {
+                  return (
+                    <tr key={i.toString()}>
+                      <td className={styles["text-align-left"]}>{username}</td>
+                      <td>
+                        <div className={styles["button-container"]}>
+                          <Button
+                            className={styles["button"]}
+                            variant={ButtonVariant.Small}
+                            onClick={() => onRemove(username)}
+                          >
+                            <RemoveIcon />
+                          </Button>
+                        </div>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        )}
+        {removedMembers.length > 0 && (
+          <>
+            <div className={styles["remove-members-container"]}>
+              <TitleWithInfo
+                title="Fjernede medlemmer"
+                infoText="Trykk på medlem for å angre"
+              />
+              <div className={styles["groups-container"]}>
+                <CheckboxCards
+                  items={removedMembers.map((username, i) => ({
+                    title: username,
+                    key: i.toString(),
+                  }))}
+                  checked={removedMembers}
+                  setChecked={setRemovedMembers}
+                />
+              </div>
+            </div>
+            <Button
+              variant={ButtonVariant.Medium}
+              color={ButtonColor.Red}
+              className={styles["primary-button"]}
+              onClick={() => onConfirm()}
+            >
+              Fjern
+            </Button>
+          </>
+        )}
       </div>
     </PageWrapper>
   );
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,4 +1,17 @@
+import { where } from "firebase/firestore";
 import { useContext } from "react";
 import { userContext } from "src/app/providers";
+import { getDocuments, usersCol } from "src/fire-base/db";
+import { User } from "src/fire-base/models";
 
-export const useUser = () => useContext(userContext)
+export const useUser = () => useContext(userContext);
+
+export const getUserId = async (userName: string) => {
+  const users = await getDocuments<User>({
+    collectionId: usersCol,
+    constraints: [where("username", "==", userName)],
+  });
+  if (users.length > 0) {
+    return users[0].id;
+  }
+};
