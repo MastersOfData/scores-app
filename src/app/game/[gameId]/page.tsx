66d2d85975422533368a64d84d830cb8fc09f2e1
andersbuanes <anderstbuanes@hotmail.com>
2023-04-18 19:20:04 +0200

    Add game action log to game page

--- src/app/game/[gameId]/page.tsx
+++ src/app/game/[gameId]/page.tsx
@@ -1,17 +1,22 @@
 "use client";
 
 import { FC, useState } from "react";
-import { Card } from "src/components/Card"
+import { Card } from "src/components/Card";
 import { Button, ButtonVariant, ButtonColor } from "src/components/Button";
-import PageWrapper from "src/components/PageWrapper"
+import PageWrapper from "src/components/PageWrapper";
 import CardStyles from "src/styles/Card.module.css";
 import ButtonStyles from "src/styles/Button.module.css";
-import SpillStyles from "src/styles/Spill.module.css"
-import { useGetGroupsForCurrentUser, useUserHasAccessToGame, useGetLiveGame } from "src/store/hooks";
+import SpillStyles from "src/styles/Spill.module.css";
+import {
+  useGetGroupsForCurrentUser,
+  useUserHasAccessToGame,
+  useGetLiveGame,
+} from "src/store/hooks";
 import GroupStyles from "src/styles/Group.module.css";
 import {
-    calculateGroupLeaderboard,
-  } from "src/utils/util";
+  calculateGroupLeaderboard,
+  getElapsedTimeStringFromSeconds,
+} from "src/utils/util";
 import Medal, { MedalType } from "src/components/Medal";
 import { RadioCards } from "src/components/RadioCards";
 import RegResultStyles from "src/styles/RegisterResult.module.css";
@@ -24,172 +29,170 @@ import { DataStatus } from "../../../store/store.types";
 import Spinner from "../../../components/Spinner";
 import { useRouter, useSearchParams } from "next/navigation";
 
-
 interface GameScreenProps {
-    params: { gameId: string };
+  params: { gameId: string };
+}
+
+const GameScreen: FC<GameScreenProps> = ({ params }) => {
+  const router = useRouter();
+  const { gameId } = params;
+  const access = useUserHasAccessToGame(gameId);
+
+  //Skummelt å loade inn game før vi vet om en bruker has access?
+  const gamesWithStatus = useGetGameById(gameId);
+  const game = gamesWithStatus.data;
+
+  const userContext = useUser();
+  const user = userContext.userData;
+  const operators = ["+", "-", "×", "÷"];
+
+  const groupsWithStatus = useGetGroupsForCurrentUser();
+
+  const liveGame = useGetLiveGame(gameId);
+
+  //Hooks
+  const [selectedUser, setSelectedUser] = useState<string | undefined>("");
+  const [isGroupGame, setIsGroupGame] = useState(true);
+  const [expression, setExpression] = useState<string>("");
+  const [currentUserPoints, setCurrentUserPoints] = useState<string>("");
+
+  if (
+    user &&
+    (groupsWithStatus.status === DataStatus.LOADING ||
+      !access.hasLoaded ||
+      groupsWithStatus.data === undefined ||
+      gamesWithStatus.status === DataStatus.LOADING ||
+      gamesWithStatus.data === undefined)
+  ) {
+    return <Spinner />;
   }
 
-const GameScreen: FC<GameScreenProps> = ({ params })=> {
-    const router = useRouter();
-    const { gameId } = params;
-    const access = useUserHasAccessToGame(gameId)
+  if (!access.hasAccess) {
+    return <p>{access.noAccessReason}</p>;
+  }
+  //Kan en se denne siden uten å logge inn?
+  if (user === null) {
+    return <div />;
+  }
+  //NOT DONE
+  if (game === undefined) {
+    return <div />;
+  }
 
-    //Skummelt å loade inn game før vi vet om en bruker has access?
-    const gamesWithStatus = useGetGameById(gameId);
-    const game = gamesWithStatus.data;
+  const userArr = [user];
 
-    const userContext = useUser();
-    const user = userContext.userData
-    const operators = ["+", "-", "×", "÷"]
-    
-    const groupsWithStatus = useGetGroupsForCurrentUser();
+  const group = groupsWithStatus.data?.find(
+    (group) => group.id === game.groupId
+  );
 
-    const liveGame = useGetLiveGame(gameId)
-
-    //Hooks
-    const [selectedUser, setSelectedUser] = useState<string | undefined>("");
-    const [isGroupGame, setIsGroupGame] = useState(true);
-    const [expression, setExpression] = useState<string>("");
-    const [currentUserPoints, setCurrentUserPoints] = useState<string>("");
-
-    if (
-      user &&
-      (groupsWithStatus.status === DataStatus.LOADING ||
-        !access.hasLoaded ||
-        groupsWithStatus.data === undefined ||
-        gamesWithStatus.status === DataStatus.LOADING ||
-        gamesWithStatus.data === undefined)
-    ) {
-      return <Spinner />;
-    }
-
-    if (!access.hasAccess){
-      return <p>{access.noAccessReason}</p>
-    }
-    //Kan en se denne siden uten å logge inn?
-    if (user === null){
-      return <div />
-    }
-    //NOT DONE
-    if (game === undefined){
-      return <div />
-    }
-
-    const userArr = [user];
-    
-    const group = groupsWithStatus.data?.find((group) => group.id === game.groupId);
-
-    const calcExpr = () => {
-      const mathExpr = expression.replace("×", "*").replace("÷", "/")
-      try{
-        if (selectedUser){
-          const newScore = eval(mathExpr).toString();
-        liveGame.addPoints(selectedUser, newScore)
+  const calcExpr = () => {
+    const mathExpr = expression.replace("×", "*").replace("÷", "/");
+    try {
+      if (selectedUser) {
+        const newScore = eval(mathExpr).toString();
+        liveGame.addPoints(selectedUser, newScore);
         setExpression(newScore);
-        }
-        else{
-          alert("User has not been defined!")
-        }
-        
+      } else {
+        alert("User has not been defined!");
       }
-      catch (err) {
-        console.log(err)
-        alert("Not a valid expression!")
-      }
+    } catch (err) {
+      console.log(err);
+      alert("Not a valid expression!");
+    }
+  };
+
+  async function setUserPoints(username: string): Promise<void> {
+    console.log("SetUserPoints");
+    if (game === undefined) {
+      return;
+    }
+    //Get score from user and setExpression(score)
+
+    setSelectedUser(username);
+
+    const userId = await getUserId(username);
+    if (userId === undefined) {
+      return;
     }
 
-    async function setUserPoints(username : string): Promise<void> {
-      console.log("SetUserPoints")
-      if (game === undefined) {
-        return 
-      }
-      //Get score from user and setExpression(score)
-
-      setSelectedUser(username);
-      
-       
-      
-    
-      const userId = await getUserId(username);
-      if (userId === undefined){
-        return
-      }
-
-      for (const score of game.players) {
-        if (score.playerId === userId) {
-          setExpression(score.toString())
-          break;
-        }
+    for (const score of game.players) {
+      if (score.playerId === userId) {
+        setExpression(score.toString());
+        break;
       }
     }
+  }
 
-    if (!group) {
-        return (
-          <PageWrapper title="" backPath="/" authenticated>
-            <div className="center-items">
-              <p>Gruppen finnes ikke! 🚨</p>
-            </div>
-          </PageWrapper>
-        );
-      }
-
-
-    const gameEmoji = "😂"
-    const gameTitle = gameEmoji + "Tennis"
-    const time = "13:37"
-    const groupMember = ["Tore", "Tang"]
-
-    const leaderboardStats = calculateGroupLeaderboard(group.members);
-
-    const onSubmit = () => {
-      console.log("It is submitted")
-    }
-
-
-   
-return (
-    <PageWrapper title='Spill' backPath='/' authenticated={true}>
-        <div className={SpillStyles["header-cards"]}>
-            <Card title = {gameTitle} />
-            <div className={`${CardStyles["card"]} 
-                            ${ButtonStyles["button--green"]}`}>
-                <div className={CardStyles["card-header-wrapper"]}>
-                    <h4 className={`${CardStyles["card-title"]} ${SpillStyles.timeLabel} `}>{time}</h4>
-                </div>
-            </div>
+  if (!group) {
+    return (
+      <PageWrapper title="" backPath="/" authenticated>
+        <div className="center-items">
+          <p>Gruppen finnes ikke! 🚨</p>
         </div>
+      </PageWrapper>
+    );
+  }
 
-        <table className={GroupStyles["leaderboard"]}>
-          <thead>
-            <tr>
-              <th>#</th>
-              <th className={GroupStyles["text-align-left"]}>Bruker</th>
-              <th>Poeng</th>
+  const gameEmoji = "😂";
+  const gameTitle = gameEmoji + "Tennis";
+  const time = "13:37";
+  const groupMember = ["Tore", "Tang"];
 
-            </tr>
-          </thead>
-          <tbody>
-            {leaderboardStats.map((member, index) => {
-              return (
-                <tr key={member.userId}>
-                  <td>
-                    {index < 3 ? (
-                      <Medal type={Object.values(MedalType)[index]} />
-                    ) : (
-                      index + 1
-                    )}
-                  </td>
-                  <td className={GroupStyles["text-align-left"]}>
-                    {member.username}
-                  </td>
-                  <td>{member.wins}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-        <h2 className={RegResultStyles["title-centered"]}>Oppdater poeng</h2>
-        {isGroupGame ? (<div>
+  const leaderboardStats = calculateGroupLeaderboard(group.members);
+
+  const onSubmit = () => {
+    console.log("It is submitted");
+  };
+
+  return (
+    <PageWrapper title="Spill" backPath="/" authenticated={true}>
+      <div className={SpillStyles["header-cards"]}>
+        <Card title={gameTitle} />
+        <div
+          className={`${CardStyles["card"]} 
+                            ${ButtonStyles["button--green"]}`}
+        >
+          <div className={CardStyles["card-header-wrapper"]}>
+            <h4
+              className={`${CardStyles["card-title"]} ${SpillStyles.timeLabel} `}
+            >
+              {time}
+            </h4>
+          </div>
+        </div>
+      </div>
+
+      <table className={GroupStyles["leaderboard"]}>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th className={GroupStyles["text-align-left"]}>Bruker</th>
+            <th>Poeng</th>
+          </tr>
+        </thead>
+        <tbody>
+          {leaderboardStats.map((member, index) => {
+            return (
+              <tr key={member.userId}>
+                <td>
+                  {index < 3 ? (
+                    <Medal type={Object.values(MedalType)[index]} />
+                  ) : (
+                    index + 1
+                  )}
+                </td>
+                <td className={GroupStyles["text-align-left"]}>
+                  {member.username}
+                </td>
+                <td>{member.wins}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <h2 className={RegResultStyles["title-centered"]}>Oppdater poeng</h2>
+      {isGroupGame ? (
+        <div>
           <div className={RegResultStyles["groups-container"]}>
             <RadioCards
               items={group.members.map((user, i) => ({
@@ -200,76 +203,112 @@ return (
               setSelected={setUserPoints}
             />
           </div>
-        </div>) : (<RadioCards
-              items={userArr.map((user, i) => ({
-                title: user.toString(),
-                key: user.toString(),
-              }))}
-              selected={selectedUser}
-              setSelected={setSelectedUser}
-            />)}
-        <div className={SpillStyles["calculator-container"]}>
-          <Input 
-              placeholder="Legg til poeng..."
-              type = {"text"}
-              value = {expression}
-              className = {SpillStyles["text-input"]}
-              onInput = {setExpression} />
-
-          <div className={SpillStyles["math-buttonsContainer"]}>
-              <Button 
-                className={SpillStyles["operator-button"]}
-                variant = {ButtonVariant.Round}
-                color = {ButtonColor.Grey}
-                onClick = {() => setExpression(expression + " + ")} 
-              > 
-                +
-              </Button>
-              <Button 
-                className={SpillStyles["operator-button"]}
-                variant = {ButtonVariant.Round}
-                color = {ButtonColor.Grey}
-                onClick = {() => setExpression(expression + " - ")} 
-              > 
-                -
-              </Button>
-              <Button 
-                className={SpillStyles["operator-button"]}
-                variant = {ButtonVariant.Round}
-                color = {ButtonColor.Grey}
-                onClick = {() => setExpression(expression + " × ")} 
-              > 
-                ×
-              </Button>
-              <Button 
-                className={SpillStyles["operator-button"]}
-                variant = {ButtonVariant.Round}
-                color = {ButtonColor.Grey}
-                onClick = {() => setExpression(expression + " ÷ ")} 
-              > 
-                ÷
-              </Button>
-          </div>
-            <Button
-              className={SpillStyles["calculate-button"]}
-              variant={ButtonVariant.Medium}
-              color={ButtonColor.Red}
-              onClick={calcExpr}
-            >
-              Regn ut
-            </Button>
         </div>
+      ) : (
+        <RadioCards
+          items={userArr.map((user, i) => ({
+            title: user.toString(),
+            key: user.toString(),
+          }))}
+          selected={selectedUser}
+          setSelected={setSelectedUser}
+        />
+      )}
+      <div className={SpillStyles["calculator-container"]}>
+        <Input
+          placeholder="Legg til poeng..."
+          type={"text"}
+          value={expression}
+          className={SpillStyles["text-input"]}
+          onInput={setExpression}
+        />
 
+        <div className={SpillStyles["math-buttonsContainer"]}>
+          <Button
+            className={SpillStyles["operator-button"]}
+            variant={ButtonVariant.Round}
+            color={ButtonColor.Grey}
+            onClick={() => setExpression(expression + " + ")}
+          >
+            +
+          </Button>
+          <Button
+            className={SpillStyles["operator-button"]}
+            variant={ButtonVariant.Round}
+            color={ButtonColor.Grey}
+            onClick={() => setExpression(expression + " - ")}
+          >
+            -
+          </Button>
+          <Button
+            className={SpillStyles["operator-button"]}
+            variant={ButtonVariant.Round}
+            color={ButtonColor.Grey}
+            onClick={() => setExpression(expression + " × ")}
+          >
+            ×
+          </Button>
+          <Button
+            className={SpillStyles["operator-button"]}
+            variant={ButtonVariant.Round}
+            color={ButtonColor.Grey}
+            onClick={() => setExpression(expression + " ÷ ")}
+          >
+            ÷
+          </Button>
+        </div>
         <Button
-          variant={ButtonVariant.Round}
-          color={ButtonColor.Green}
-          onClick={onSubmit}
+          className={SpillStyles["calculate-button"]}
+          variant={ButtonVariant.Medium}
+          color={ButtonColor.Red}
+          onClick={calcExpr}
         >
-          Start spill
+          Regn ut
         </Button>
-
+      </div>
+      {liveGame.localGameLog.length > 0 ? (
+        <>
+          <h2 className={RegResultStyles["title-centered"]}>Logg</h2>
+          <table className={SpillStyles["log"]}>
+            <thead>
+              <tr>
+                <th>Tid</th>
+                <th className={SpillStyles["text-align-left"]}>Bruker</th>
+                <th>Tildeler</th>
+                <th>Poeng</th>
+              </tr>
+            </thead>
+            <tbody>
+              {liveGame.localGameLog.map((entry, index) => {
+                const { subjectId, actorId, timestamp, value } = entry;
+                return (
+                  <tr key={index + entry.actionType + entry.subjectId}>
+                    <td>
+                      {getElapsedTimeStringFromSeconds(timestamp.seconds)}
+                    </td>
+                    <td className={SpillStyles["text-align-left"]}>
+                      {group?.members.find((m) => m.id === subjectId)?.username}
+                    </td>
+                    <td>
+                      {group?.members.find((m) => m.id === actorId)?.username}
+                    </td>
+                    <td>{value}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </>
+      ) : null}
+      <Button
+        variant={ButtonVariant.Round}
+        color={ButtonColor.Green}
+        onClick={onSubmit}
+      >
+        Start spill
+      </Button>
     </PageWrapper>
-    )
-}
+  );
+};
 
 export default GameScreen;
--- src/app/test-live-game/page.tsx
+++ src/app/test-live-game/page.tsx
@@ -6,6 +6,8 @@ import Spinner from "../../components/Spinner";
 import { GameActionType } from "../../types/types";
 import { getElapsedTimeStringFromSeconds } from "../../utils/util";
 
+import Styles from "../../styles/Spill.module.css";
+
 export default function TestLiveGame() {
   const gameId = "cb4Xqs6WdfUfKVHHVygm";
 
@@ -16,16 +18,16 @@ export default function TestLiveGame() {
     live.elapsedGameTime
   );
 
-  if (!gameAccess.hasLoaded) {
-    return <Spinner />;
-  }
+  // if (!gameAccess.hasLoaded) {
+  //   return <Spinner />;
+  // }
 
-  if (!gameAccess.hasAccess) {
-    return <p>{gameAccess.noAccessReason}</p>;
-  }
+  // if (!gameAccess.hasAccess) {
+  //   return <p>{gameAccess.noAccessReason}</p>;
+  // }
 
   return (
-    <PageWrapper title='Live game test' authenticated={true}>
+    <PageWrapper title="Live game test" authenticated={true}>
       <p>{elapsedTimeString}</p>
       <button onClick={() => live.changeGameStatus(GameActionType.START)}>
         Start game
@@ -34,15 +36,36 @@ export default function TestLiveGame() {
         Finish game
       </button>
       <button onClick={() => live.addPoints("[userid]", 3)}>Add points</button>
-      <h2>Log</h2>
-      {live.localGameLog
-        .sort((a, b) => b.timestamp.seconds - a.timestamp.seconds)
-        .map((log) => (
-          <p key={log.id}>
-            {log.timestamp.toDate().toLocaleTimeString()},
-            {log.actionType.toString()}, {log.value}, {log.subjectId}
-          </p>
-        ))}
+      <h2>Logg</h2>
+      <div>
+        <table className={Styles["log"]}>
+          <thead>
+            <tr>
+              <th>Tid</th>
+              <th className={Styles["text-align-left"]}>Bruker</th>
+              <th className={Styles["text-align-left"]}>Tildeler</th>
+              <th>Poeng</th>
+            </tr>
+          </thead>
+          <tbody>
+            {live.localGameLog
+              .sort((a, b) => b.timestamp.seconds - a.timestamp.seconds)
+              .map((entry, index) => {
+                const { subjectId, actorId, timestamp, value } = entry;
+                return (
+                  <tr key={index + entry.actionType + entry.subjectId}>
+                    <td>
+                      {getElapsedTimeStringFromSeconds(timestamp.seconds)}
+                    </td>
+                    <td className={Styles["text-align-left"]}>{subjectId}</td>
+                    <td className={Styles["text-align-left"]}>{actorId}</td>
+                    <td>{value}</td>
+                  </tr>
+                );
+              })}
+          </tbody>
+        </table>
+      </div>
     </PageWrapper>
   );
 }
