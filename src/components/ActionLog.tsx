9a28103a06cc0f922e02fb5ac454bc667f567691
Rune Mæstad <54098894+rune-m@users.noreply.github.com>
2023-04-25 15:48:56 +0200

    Game log styling progress (#100)

--- src/components/ActionLog.tsx
+++ src/components/ActionLog.tsx
@@ -34,6 +34,30 @@ const mockActions: GameAction[] = [
     subjectId: "7Om4PoQLCbdQOIhatXZAlfzABdF2",
     value: -10,
   },
+  {
+    gameId: "69",
+    actorId: "7Om4PoQLCbdQOIhatXZAlfzABdF2",
+    actionType: GameActionType.FINISH,
+    timestamp: Timestamp.fromDate(new Date()),
+    subjectId: "7Om4PoQLCbdQOIhatXZAlfzABdF2",
+    value: -10,
+  },
+  {
+    gameId: "69",
+    actorId: "7Om4PoQLCbdQOIhatXZAlfzABdF2",
+    actionType: GameActionType.FINISH,
+    timestamp: Timestamp.fromDate(new Date()),
+    subjectId: "7Om4PoQLCbdQOIhatXZAlfzABdF2",
+    value: -10,
+  },
+  {
+    gameId: "69",
+    actorId: "7Om4PoQLCbdQOIhatXZAlfzABdF2",
+    actionType: GameActionType.FINISH,
+    timestamp: Timestamp.fromDate(new Date()),
+    subjectId: "7Om4PoQLCbdQOIhatXZAlfzABdF2",
+    value: -10,
+  },
 ];
 
 export const ActionLog: FC<ActionLogProps> = ({ actions }) => {
@@ -42,39 +66,42 @@ export const ActionLog: FC<ActionLogProps> = ({ actions }) => {
     [mockActions]
   );
   return (
-    <div className={styles["log-wrapper"]}>
-      <table className={styles["table"]}>
-        <thead>
-          <tr>
-            <th>Tid</th>
-            <th>Bruker</th>
-            <th>Tildeler</th>
-            <th>Poeng</th>
-          </tr>
-        </thead>
-        <tbody>
-          {mockActions.map((action, index) => (
-            <tr key={index} className={styles["tr"]}>
-              <td className={styles["td"]}>
-                {formatSeconds(action.timestamp.seconds - startTime)}
-              </td>
-              <td className={styles["td"]}>Birger</td>
-              <td className={styles["td"]}>Birger</td>
-              {action.value && (
-                <td className={styles["td"]}>
-                  {action.value > 0 ? (
-                    <p
-                      className={styles["positive-value"]}
-                    >{`+${action.value}`}</p>
-                  ) : (
-                    <p className={styles["negative-value"]}>{action.value}</p>
-                  )}
-                </td>
-              )}
+    <div className={styles.container}>
+      <h2>Spillogg</h2>
+      <div className={styles["log-wrapper"]}>
+        <table className={styles["table"]}>
+          <thead className={styles["thead"]}>
+            <tr>
+              <th className={styles["th"]}>Tid</th>
+              <th className={styles["th"]}>Bruker</th>
+              <th className={styles["th"]}>Tildeler</th>
+              <th className={styles["th"]}>Poeng</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody className={styles["tbody"]}>
+            {mockActions.map((action, index) => (
+              <tr key={index} className={styles["tr"]}>
+                <td className={styles["td"]}>
+                  {formatSeconds(action.timestamp.seconds - startTime)}
+                </td>
+                <td className={styles["td"]}>Birger</td>
+                <td className={styles["td"]}>Birger</td>
+                {action.value && (
+                  <td className={styles["td"]}>
+                    {action.value > 0 ? (
+                      <p
+                        className={styles["positive-value"]}
+                      >{`+${action.value}`}</p>
+                    ) : (
+                      <p className={styles["negative-value"]}>{action.value}</p>
+                    )}
+                  </td>
+                )}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
